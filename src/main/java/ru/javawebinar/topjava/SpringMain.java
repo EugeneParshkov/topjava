2a7c6daa49a7e0b2804d3340daab4fb95845de78
EugeneParshkov <parshkovea@gmail.com>
2023-02-18 02:59:23 +0300

    implement InMemoryUserRepository

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -4,17 +4,33 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
 import ru.javawebinar.topjava.web.user.AdminRestController;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class SpringMain {
     public static void main(String[] args) {
         // java 7 automatic resource management (ARM)
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
-            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
-        }
+//        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
+//            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+//            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+//            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
+
+
+//        }
+        User user = new User(null, "userName", "email@mail.ru", "password", Role.ADMIN);
+        User user1 = new User(null, "aserName", "email@mail.ru", "password", Role.ADMIN);
+        User user2 = new User(null, "userName", "amail@mail.ru", "password", Role.ADMIN);
+        UserRepository repository = new InMemoryUserRepository();
+        repository.save(user);
+        repository.save(user1);
+        repository.save(user2);
+        List <User> list = repository.getAll();
+
+        list.forEach(System.out::println);
+
     }
 }
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -6,40 +6,67 @@ import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
+    private final Map<Integer, User> userRepository = new ConcurrentHashMap<>();
+    private final AtomicInteger counter = new AtomicInteger(0);
 
     @Override
     public boolean delete(int id) {
         log.info("delete {}", id);
-        return true;
+        return userRepository.remove(id) != null;
     }
 
     @Override
     public User save(User user) {
         log.info("save {}", user);
-        return user;
+        if (user.isNew()) {
+            user.setId(counter.incrementAndGet());
+            userRepository.put(user.getId(), user);
+            return user;
+        }
+        return userRepository.computeIfPresent(user.getId(), (id,oldUser) -> user);
     }
 
     @Override
     public User get(int id) {
         log.info("get {}", id);
-        return null;
+        return userRepository.get(id);
     }
 
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+        class UserNameComparator implements Comparator<User> {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        }
+        class UserEmailComparator implements Comparator<User> {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getEmail().compareTo(o2.getEmail());
+            }
+        }
+        Comparator<User> comparator = new UserNameComparator()
+                .thenComparing(new UserEmailComparator());
+        List<User> users = new ArrayList<>(userRepository.values());
+        users.sort(comparator);
+
+        return users;
     }
 
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
-        return null;
+       return userRepository.values().stream().
+               filter(user -> user.getEmail().equals(email)).findFirst().orElse(null);
+
     }
 }

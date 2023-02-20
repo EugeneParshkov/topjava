eeaa2d64bfda7c3581a292ba6705701bfa976a39
EugeneParshkov <parshkovea@gmail.com>
2023-02-20 04:06:04 +0300

    HW02 end

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -2,12 +2,16 @@ package ru.javawebinar.topjava;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
+import ru.javawebinar.topjava.web.meal.MealRestController;
 import ru.javawebinar.topjava.web.user.AdminRestController;
 
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,19 +22,23 @@ public class SpringMain {
 //            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
 //            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
 //            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
-
-
 //        }
-        User user = new User(null, "userName", "email@mail.ru", "password", Role.ADMIN);
-        User user1 = new User(null, "aserName", "email@mail.ru", "password", Role.ADMIN);
-        User user2 = new User(null, "userName", "amail@mail.ru", "password", Role.ADMIN);
-        UserRepository repository = new InMemoryUserRepository();
-        repository.save(user);
-        repository.save(user1);
-        repository.save(user2);
-        List <User> list = repository.getAll();
+        try (ConfigurableApplicationContext appCpx = new ClassPathXmlApplicationContext("spring/spring-app.xml")){
+            System.out.println("Bean definition names: " + Arrays.toString(appCpx.getBeanDefinitionNames()));
+            MealRestController mealRestController = appCpx.getBean(MealRestController.class);
+            mealRestController.create(new Meal(LocalDateTime.of(2023, Month.JANUARY, 30, 10, 0), "Завтрак", 1000));
+        }
+//        User user = new User(null, "userName", "email@mail.ru", "password", Role.ADMIN);
+//        User user1 = new User(null, "aserName", "email@mail.ru", "password", Role.ADMIN);
+//        User user2 = new User(null, "userName", "amail@mail.ru", "password", Role.ADMIN);
+//        UserRepository repository = new InMemoryUserRepository();
+//        repository.save(user);
+//        repository.save(user1);
+//        repository.save(user2);
+//        List <User> list = repository.getAll();
+//
+//        list.forEach(System.out::println);
 
-        list.forEach(System.out::println);
-
+        }
     }
-}
+
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -2,8 +2,8 @@ package ru.javawebinar.topjava.repository.inmemory;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 import ru.javawebinar.topjava.web.SecurityUtil;
@@ -15,11 +15,11 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
+@Repository
 public class InMemoryMealRepository implements MealRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryMealRepository.class);
     private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
-    private User user;
 
     {
         int id = SecurityUtil.authUserId();
